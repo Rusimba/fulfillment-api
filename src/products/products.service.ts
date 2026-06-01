871c09d6406db2b77d6948a092c47b83e317b4f4
Rusiamba <vyav_yav@mail.ru>
2026-06-01 20:40:57 +0300

    a

---
+++ src/products/products.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from '../prisma.service'; // <-- Подключаем Присму
+
+@Injectable()
+export class ProductsService {
+  // Внедряем Присму в класс
+  constructor(private readonly prisma: PrismaService) {}
+
+  // Метод создания товара
+  create(createProductDto: CreateProductDto) {
+    return this.prisma.product.create({
+      data: createProductDto,
+    });
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
